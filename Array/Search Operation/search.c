1d5b59616147034974122a017f53c622f3c5a5fe
mostakinahmed <mostakin4556@gmail.com>
2024-10-27 23:54:49 +0600

    c

--- Array/Search Operation/search.c
+++ Array/Search Operation/search.c
@@ -1,19 +1,19 @@
-#include <stdio.h>
+#include <stdio.h> 
+#include <stdlib.h>
+
 int main()
 {
-  int array[100], search, c, n;
+  int search, c, n;
   printf("Enter number of elements in array\n");
   scanf("%d", &n);
 
-
+  int array[n];
   printf("Enter %d integer(s)\n", n);
   for (c = 0; c < n; c++)
-  scanf("%d", &array[c]);
-
+    scanf("%d", &array[c]);
 
   printf("Enter a number to search\n");
   scanf("%d", &search);
-
 
   for (c = 0; c < n; c++)
   {
@@ -22,10 +22,11 @@ int main()
       printf("%d is present at location %d.\n", search, c+1);
       break;
     }
-    
   }
-  if (c == n){
-    printf("%d isn't present in the array.\n", search);
+
+  if (c==n)
+  {
+    printf("%d isn't present in array.\n", search);
   }
 
   return 0;
