ef8a42cb5c3e05e1bb3fa226122a26f6f2507349
mostakinahmed <mostakin4556@gmail.com>
2024-11-24 23:26:39 +0600

    ll

--- Array/2D Array/program.c
+++ Array/2D Array/program.c
@@ -5,8 +5,7 @@ int main() {
       // and 2 columns
     int arr[3][2] = { { 0, 1 }, { 2, 3 }, { 4, 5 } };
 
-    // Print each array 
-    //using loop
+ //loop
     for (int i = 0; i < 3; i++) {
         for (int j = 0; j < 2; j++) {
             printf("arr[%d][%d]: %d    ", i, j, arr[i][j]);
