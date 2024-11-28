ae3cd7ea9b4dfa6ef844c9889ce5a9c3b11af0c0
mostakinahmed <mostakin4556@gmail.com>
2024-11-28 20:32:12 +0600

    aa

--- Array/Delete Operation/delete.c
+++ Array/Delete Operation/delete.c
@@ -6,15 +6,18 @@ int main() {
 
     int positionToDelete = 4; 
 
+printf("Original array: ");
     // Shift elements to the left to overwrite the element at the specified position
     for (int i = positionToDelete; i < size - 1; i++) {
         arr[i] = arr[i + 1];
     }
 // Decrement the size of the array to reflect the deletion
+//mostakin
     size--;
 
     for (int i = 0; i < size; i++) {
         printf("%d ", arr[i]);
     }
     return 0;
+
 }
