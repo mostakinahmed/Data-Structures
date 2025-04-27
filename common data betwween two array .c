f73b0d78c8cbb10e4947f85043be3d0ec7421227
Mostakin Ahmed <105800326+mostakinahmed@users.noreply.github.com>
2025-04-27 20:19:50 +0600

    Create common data betwween two array .c

---
+++ common data betwween two array .c
@@ -0,0 +1,54 @@
+// Linear Algorithms
+#include <stdio.h>
+
+int revArray(int data1[], int data2[], int size)
+
+{
+    printf("\nCommon Data: \n");
+    for (int i = 0; i < size; i++)
+    {
+        for (int j = 0; j < size; j++)
+        {
+            if (data1[i] == data2[j])
+            {
+                printf("%d ", data1[i]);
+            }
+        }
+    }
+}
+
+int main()
+{
+    int size, s;
+    printf("Enter size: ");
+    scanf("%d", &size);
+    int data1[size], data2[size];
+
+    printf("\n1st array: \n");
+    for (int i = 0; i < size; i++)
+    {
+        printf("data[%d]: ", i);
+        scanf("%d", &data1[i]);
+    }
+    printf("\n2nd array: \n");
+    for (int i = 0; i < size; i++)
+    {
+        printf("data[%d]: ", i);
+        scanf("%d", &data2[i]);
+    }
+    // print original array
+    printf("\noriginal array 1: \n");
+    for (int i = 0; i < size; i++)
+    {
+        printf("%d ", data1[i]);
+    }
+    printf("\noriginal array 2: \n");
+    for (int i = 0; i < size; i++)
+    {
+        printf("%d ", data2[i]);
+    }
+
+    revArray(data1, data2, size);
+
+    return 0;
+}
