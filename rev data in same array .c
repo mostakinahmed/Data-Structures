9c321f9e9c9f6a5f4738430bd963c4c3e0b14150
Mostakin Ahmed <105800326+mostakinahmed@users.noreply.github.com>
2025-04-27 18:54:00 +0600

    Create rev data in same array .c

---
+++ rev data in same array .c
@@ -0,0 +1,45 @@
+// Linear Algorithms
+#include <stdio.h>
+
+int revArray(int data[], int size)
+{
+    int temp;
+    for (int i = 0; i < size / 2; i++)
+    {
+        temp = 0;
+        temp = data[i];
+        data[i] = data[size - i - 1];
+        data[size - i - 1] = temp;
+        // data[size - i - 1] = data[i];
+    }
+
+    // print rev array
+    printf("\nreverse array: \n");
+    for (int i = 0; i < size; i++)
+    {
+        printf("%d ", data[i]);
+    }
+}
+int main()
+{
+    int size, s;
+    printf("Enter size: ");
+    scanf("%d", &size);
+    int data[size];
+
+    for (int i = 0; i < size; i++)
+    {
+        printf("data[%d]: ", i);
+        scanf("%d", &data[i]);
+    }
+    // print original array
+    printf("original array: \n");
+    for (int i = 0; i < size; i++)
+    {
+        printf("%d ", data[i]);
+    }
+
+    revArray(data, size);
+
+    return 0;
+}
