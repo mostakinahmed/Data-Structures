47fdf84cea869576857e605da1ca2b4a895d7b08
Mostakin Ahmed <105800326+mostakinahmed@users.noreply.github.com>
2025-07-18 16:44:25 +0600

    Create add between node .c

---
+++ Linked LIst/doubly linked list/add between node .c
@@ -0,0 +1,89 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node
+{
+    int data;
+    struct Node *next;
+    struct Node *prev;
+};
+
+int main()
+{
+    struct Node *head = NULL;
+    struct Node *first = NULL;
+    struct Node *second = NULL;
+    struct Node *third = NULL;
+    struct Node *previousNode = NULL;
+
+    first = (struct Node *)malloc(sizeof(struct Node));
+    second = (struct Node *)malloc(sizeof(struct Node));
+    third = (struct Node *)malloc(sizeof(struct Node));
+
+    // data assign
+    first->data = 10;
+    first->next = second;
+    first->prev = NULL;
+
+    second->data = 20;
+    second->next = third;
+    second->prev = first;
+
+    third->data = 30;
+    third->next = NULL;
+    third->prev = second;
+
+    struct Node *current = head;
+    head = first;
+    printf("\nOriginal Doubly Linked List: \n");
+    current = head;
+    printf("Old     : ");
+    while (current != NULL)
+    {
+        printf("%d ", current->data);
+        current = current->next;
+    }
+
+    // create new node
+    struct Node *newNode = NULL;
+    newNode = (struct Node *)malloc(sizeof(struct Node));
+    newNode->data = 555;
+    newNode->next = NULL;
+    newNode->prev = NULL;
+
+    // adding at any position
+    int pos;
+    printf("\nEnter position: ");
+    scanf("%d", &pos);
+    current = head;
+    while (pos != 1)
+    {
+        previousNode = current;
+        current = current->next;
+        pos--;
+    }
+    previousNode->next = newNode;
+    newNode->prev = previousNode;
+    newNode->next = current;
+    current->prev = newNode;
+
+    // printf last list
+    current = head;
+    printf("\nNew List: ");
+    while (current != NULL)
+    {
+        printf("%d ", current->data);
+        current = current->next;
+    }
+
+    printf("\n");
+    current = third;
+    printf("Backward: ");
+    while (current != NULL)
+    {
+        printf("%d ", current->data);
+        current = current->prev;
+    }
+
+    return 0;
+}
